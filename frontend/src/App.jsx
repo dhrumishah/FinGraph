f2cc47cce1c39ed6c5fb71a094523396ed00ddaa
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-07-08 15:03:13 +0530

    integration of add-income on client side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,29 @@ import { MainLayout } from "./styles/Layouts";
 import Orb from "./components/Orb/Orb";
 import Navigation from "./components/Navigation/Navigation";
 import { useMemo, useState } from "react";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Income from "./components/Incomes/Income";
+import Expenses from "./components/Expenses/Expenses";
+import { useGlobalContext } from "./context/globalContext";
 
 function App() {
   const [active, setActive] = useState(1);
+  const { addIncome } = useGlobalContext();
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
+    }
+  };
   const orbMemo = useMemo(() => {
     return <Orb />;
   }, []);
@@ -16,6 +36,7 @@ function App() {
       {orbMemo}
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   );
@@ -25,6 +46,17 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${(props) => props.bg});
   position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { GlobalStyle } from "./styles/GlobalStyle.js";
 import App from "./App.jsx";
 import "./index.css";
+import { GlobalProvider } from "./context/globalContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
